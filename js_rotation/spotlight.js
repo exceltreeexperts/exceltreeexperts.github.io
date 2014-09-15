4cf93865ffd9ff59b62b5b72b096f793a0454fd9
Peter Jackson <pete@peteonrails.com>
2014-09-15 12:03:49 -0400

    Initial Screen Scrape

---
+++ js_rotation/spotlight.js
@@ -0,0 +1,13 @@
+﻿var current = "";
+var rotateInterval = 10000;
+var useEffect = false;
+
+function RunSpotlight() {
+	$("#SpotlightWrapper > ol").after('<div id="SpotlightNavigation">').cycle({fx: 'fade', pause: 1, timeout: rotateInterval, pager: '#SpotlightNavigation'});
+}
+
+function RotateSpotlights() {
+//	rotator = new SpotlightRotator();
+//	rotator.Rotate();
+}
+
